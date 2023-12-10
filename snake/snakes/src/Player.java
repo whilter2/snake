065e269347acc9e0e0ad0752aa42f7f581c4ffe9
StaleExam19 <staleexam19@gmail.com>
2023-12-10 14:56:07 +0800

    Added two player feature

---
+++ snake/snakes/src/Player.java
@@ -0,0 +1,25 @@
+public class Player {
+    int score;
+    boolean isGameOver;
+
+    Player() {
+        isGameOver = false;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public void setGameOver(boolean isGameOver) {
+        this.isGameOver = isGameOver;
+    }
+
+}
